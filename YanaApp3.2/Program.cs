70ecaa7e48c8bbd053a0cfe5d54dfc86a5894384
YanaSazhaeva <SazhaevaYana@gmail.com>
2020-02-27 21:44:03 +0300

    it works

--- YanaApp3.2/Program.cs
+++ YanaApp3.2/Program.cs
@@ -13,48 +13,46 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            double b = 0, a = 0, c = 0;
+            double b, a, c;
             Console.Write("Ввести коэффициент а: ");
             a = Convert.ToDouble(Console.ReadLine());
+            if (a == 0) {
+                Console.WriteLine("Уравнение не является квадратным");
+                Environment.Exit(0);
+            }
+                
+            else
+            {               
             Console.Write("Ввести коэффициент b: ");
             b = Convert.ToDouble(Console.ReadLine());
             Console.Write("Ввести коэффициент c: ");
             c = Convert.ToDouble(Console.ReadLine());
-            // извращения для записи уравнения
-            string str = "";
-            if (a == 0) 
-                Console.WriteLine("Уравнение не является квадратным");
-            else
-            if (b == 0)
-                Console.WriteLine("Уравнение не является квадратным");
-            else
-            if (a == 1) str += "x^2";
-                else
-                {
-                    if (a == -1) str += "-x^2";
-                    else str += a.ToString() + "x^2";
-                }
-
-            if (b == 1) str += "+x";
-                else
-                {
-                    if (b == -1) str += "-x";
-                    else str += b.ToString() + "x";
-                }
-
-            if (c == 0) str += "";
-            else
-            {
-                if (c>0) str += "+"+ c.ToString();
-                else  str += c.ToString();
-             }
             
-            Console.Write("уравнение:" + str);
+            string str = "";
 
-
-
-
-
+                str += a.ToString() + "x^2";
+                if (b == 0)
+                    str += "";
+                else
+                {
+                    if (b>0)
+                    str += " + " + b.ToString() + "x";
+                    else {
+                        str += b.ToString() + "x";
+                    }
+                }
+                if (c == 0) 
+                    str += "";
+                else
+                {
+                    if (c>0) 
+                        str += " + "+ c.ToString();
+                    else {
+                        str += c.ToString();
+                    }
+                }
+                Console.Write("уравнение:" + str);
+            }   
 
         }
     }
