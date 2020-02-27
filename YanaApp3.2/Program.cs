bce82f21a8e564490b654d0474792314454a18e6
YanaSazhaeva <SazhaevaYana@gmail.com>
2020-02-28 00:00:56 +0300

    all but without disper

--- YanaApp3.2/Program.cs
+++ YanaApp3.2/Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            double b, a, c;
+            double b, a, c, d1, d2;
             Console.Write("Ввести коэффициент а: ");
             a = Convert.ToDouble(Console.ReadLine());
             if (a == 0) {
@@ -27,6 +27,10 @@ namespace ConsoleApplication1
             b = Convert.ToDouble(Console.ReadLine());
             Console.Write("Ввести коэффициент c: ");
             c = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ввести крайнюю левую точку: ");
+            d1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ввести крайнюю правую точку: ");
+            d2 = Convert.ToDouble(Console.ReadLine());
             
             string str = "";
 
@@ -51,7 +55,32 @@ namespace ConsoleApplication1
                         str += c.ToString();
                     }
                 }
-                Console.Write("уравнение:" + str);
+                Console.Write("Уравнение: " + str + "=0\n");
+
+            string derivative = "";
+
+                if (b>0) derivative += a*2 + "x" + "+" + b ;
+                else{
+                    if (b==0) derivative += a*2 + "x";
+                    else {
+                        derivative += a*2 + "x" + b ;
+                    }
+                }   
+                Console.Write("Производная: " + derivative + "\n");
+                
+
+            double extr;
+            if (b==0) extr = 0;
+            else extr = -b/(2*a);
+            Console.Write("Экстремум: " + extr + "\n");
+
+            double integral;
+            integral = ((a*d2*d2*d2)/3 + (b*d2*d2)/2 + c*d2) - ((a*d1*d1*d1)/3 + (b*d1*d1)/2 + c*d1);
+            Console.Write("Определенный интеграл: " + Math.Round(integral, 3) + "\n");
+
+            double middle;
+            middle = integral/(d2-d1);
+            Console.Write("Среднее значение: " + Math.Round(middle, 3) + "\n");
             }   
 
         }
